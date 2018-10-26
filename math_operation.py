2f409ed44e772584ff04b86590371b9a673b2480
Jen Son <jenson0104@gmail.com>
2018-10-25 20:55:10 -0700

    Add basic math application

---
+++ math_operation.py
@@ -0,0 +1,22 @@
+number = input("Please enter some number!")
+
+times_two = number * 2
+print(f"times_two of the number '{number}' is '{times_two}'")
+
+power_three = number ** 3
+print(f"power_three of the number '{number}' is '{power_three}'")
+
+add_four = number + 4
+print(f"add_four of the number '{number}' is '{add_four}'")
+
+subtract_five = number - 5
+print(f"subtract_five of the number '{number}' is '{subtract_five}'")
+
+divide_by_six = number / 6
+print(f"divide_by_six of the number '{number}' is '{divide_by_six}'")
+
+remainder_divide_by_six = number % 6
+print(f"remainder_divide_by_six of the number '{number}' is '{remainder_divide_by_six}'")
+
+
+print("BYE :)")
